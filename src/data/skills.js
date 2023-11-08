dfdeb05a9783faad3939ba9d2a72f023d4eb8345
David Lora <lora.david2096@gmail.com>
2023-11-07 19:43:06 -0500

    Reverted all padding changes

---
+++ src/data/skills.js
@@ -0,0 +1,67 @@
+import JavaScript from "../assets/javascript.png";
+import CSS from "../assets/css.png";
+import HTML from "../assets/html.png";
+import Node from "../assets/node.png";
+import ReactImg from "../assets/react.png";
+import Tailwind from "../assets/tailwind.png";
+import ASPNET from "../assets/ASPNET.png";
+import RESTfull from "../assets/restfullapi.png";
+
+export const skills = [
+  {
+    id: 1,
+    name: "HTML",
+    image: HTML,
+    alt: "HTML icon",
+    mssg: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae dolorum, laudantium nulla possimus voluptates expedita",
+  },
+  {
+    id: 1,
+    name: "CSS",
+    image: CSS,
+    alt: "CSS icon",
+    mssg: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae dolorum, laudantium nulla possimus voluptates expedita",
+  },
+  {
+    id: 1,
+    name: "JavaScript",
+    image: JavaScript,
+    alt: "HTML icon",
+    mssg: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae dolorum, laudantium nulla possimus voluptates expedita",
+  },
+  {
+    id: 1,
+    name: "Node",
+    image: Node,
+    alt: "Node icon",
+    mssg: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae dolorum, laudantium nulla possimus voluptates expedita",
+  },
+  {
+    id: 1,
+    name: "React",
+    image: ReactImg,
+    alt: "React icon",
+    mssg: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae dolorum, laudantium nulla possimus voluptates expedita",
+  },
+  {
+    id: 1,
+    name: "Tailwind",
+    image: Tailwind,
+    alt: "HTML icon",
+    mssg: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae dolorum, laudantium nulla possimus voluptates expedita",
+  },
+  {
+    id: 1,
+    name: ".NET",
+    image: ASPNET,
+    alt: ".NET icon",
+    mssg: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae dolorum, laudantium nulla possimus voluptates expedita",
+  },
+  {
+    id: 1,
+    name: "RESTfull APIs",
+    image: RESTfull,
+    alt: "RESTfull icon",
+    mssg: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae dolorum, laudantium nulla possimus voluptates expedita",
+  },
+];
