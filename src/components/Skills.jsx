0df610d20a62c503ccc37a1b85128f5fb96e73bb
David Lora <lora.david2096@gmail.com>
2023-11-07 20:05:51 -0500

    Reverted all padding changes*5

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,7 +6,7 @@ const Skills = () => {
   return (
     <div name="skills" className={`w-full h-screen bg-[#22245D] text-white`}>
       {/* container */}
-      <div className="max-w-[1000px] mx-auto py-25 px-4 flex flex-col justify-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
           <p
             className={`text-4xl font-bold inline border-b-4 border-[#D02F2F]`}
